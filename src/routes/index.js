ff100dd3a7f9f36989080b2928b735aaf73ce67a
Willie Wheeler <willie.wheeler@gmail.com>
2017-10-14 20:05:05 -0700

    Removed a lot of stuff I'm not using

--- src/routes/index.js
+++ src/routes/index.js
@@ -17,35 +17,7 @@ const routes = {
   children: [
     {
       path: '',
-      load: () => import(/* webpackChunkName: 'home' */ './dashboard'),
-    },
-    {
-      path: '/dashboard',
       load: () => import(/* webpackChunkName: 'dashboard' */ './dashboard'),
-    },
-    {
-      path: '/contact',
-      load: () => import(/* webpackChunkName: 'contact' */ './contact'),
-    },
-    {
-      path: '/login',
-      load: () => import(/* webpackChunkName: 'login' */ './login'),
-    },
-    {
-      path: '/register',
-      load: () => import(/* webpackChunkName: 'register' */ './register'),
-    },
-    {
-      path: '/about',
-      load: () => import(/* webpackChunkName: 'about' */ './about'),
-    },
-    {
-      path: '/privacy',
-      load: () => import(/* webpackChunkName: 'privacy' */ './privacy'),
-    },
-    {
-      path: '/admin',
-      load: () => import(/* webpackChunkName: 'admin' */ './admin'),
     },
 
     // Wildcard routes, e.g. { path: '(.*)', ... } (must go last)
